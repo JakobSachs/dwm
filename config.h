e6f75d53b53e83606a08e9537d78ea18dfc3d58e
Jakob Sachs <jakobsachs1999@gmail.com>
2022-09-28 12:25:17 +0200

    Changed font

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx = 15;
 static const unsigned int snap = 32; /* snap pixel */
 static const int showbar = 1;        /* 0 means no bar */
 static const int topbar = 1;         /* 0 means bottom bar */
-static const char *fonts[] = {"Hasklig:size=15"};
+static const char *fonts[] = {"JetBrains Mono:size=15"};
 static const char col_gray1[] = "#1b1b1d";
 static const char col_gray2[] = "#242426";
 static const char col_gray3[] = "#acacac";
